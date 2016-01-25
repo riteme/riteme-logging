788f76ca807e74da98a9fd0fc494f54739be18c9
riteme <1412803389@qq.com>
2016-01-25 08:25:32 +0800

    Prepared thirdparty: cppformat

---
+++ src/thirdparty/cppformat.hpp
@@ -0,0 +1,8 @@
+#ifndef THRIDPARTY_CPPFORMAT_HPP_
+#define THRIDPARTY_CPPFORMAT_HPP_
+
+#define FMT_HEADER_ONLY
+#include "cppformat/format.h"
+#undef FMT_HEADER_ONLY
+
+#endif  // THRIDPARTY_CPPFORMAT_HPP_
